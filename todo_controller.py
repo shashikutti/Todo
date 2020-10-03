a787aecabdfc18a4bb373449c23f3a6cbebae87d
vidyaschools <sasikutti@gmail.com>
2020-10-03 13:19:24 -0700

    Initial commit

---
+++ todo_controller.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
